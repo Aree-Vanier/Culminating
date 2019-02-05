fad37c8b64dfd360833bd8552a0660dce12784f5
Aree Vanier <gregory@ajayinkingston.com>
2019-02-05 12:22:59 -0500

    Created mountPoint class

--- core/src/ca/gkelly/culminating/vessels/Vessel.java
+++ core/src/ca/gkelly/culminating/vessels/Vessel.java
@@ -3,6 +3,7 @@ package ca.gkelly.culminating.vessels;
 public class Vessel {
 	int[][] mountPoints;
 	
+	public Texture texture;
 	
 	public Vessel(int[][] mountPoints) {
 		this.mountPoints = mountPoints;
