d58c22ad3265a7467636ec54225f63135102f3e2
aree-vanier <gregory@ajayinkingston.com>
2019-02-07 14:05:22 -0500

    Added camrea code

--- core/src/ca/gkelly/culminating/Main.java
+++ core/src/ca/gkelly/culminating/Main.java
@@ -2,7 +2,9 @@ package ca.gkelly.culminating;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
@@ -16,8 +18,17 @@ public class Main extends ApplicationAdapter {
 	
 	Ship test;
 	
+	OrthographicCamera camera;
+	
+	public static final int WIDTH = 1280;
+	public static final int HEIGHT = 720;
+	
 	@Override
 	public void create () {
+		
+		camera = new OrthographicCamera(WIDTH, HEIGHT);
+		camera.setToOrtho(false, WIDTH, HEIGHT);
+		
 		batch = new SpriteBatch();
 		img = new Texture("badlogic.jpg");
 		
@@ -37,6 +48,8 @@ public class Main extends ApplicationAdapter {
 		
 		Gdx.gl.glClearColor(1, 1, 1, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		batch.setProjectionMatrix(camera.combined);
 		batch.begin();
 //		batch.draw(img, 0, 0);
 		
@@ -44,12 +57,29 @@ public class Main extends ApplicationAdapter {
 		
 		batch.end();
 		
+		int cameraX = 0;
+		int cameraY = 0;
 		
+		if(getKey(Input.Keys.LEFT) || getKey(Input.Keys.A))
+			cameraX --;
+		if(getKey(Input.Keys.RIGHT) || getKey(Input.Keys.D))
+			cameraX ++;
+		if(getKey(Input.Keys.UP) || getKey(Input.Keys.W))
+			cameraY ++;
+		if(getKey(Input.Keys.DOWN) || getKey(Input.Keys.S))
+			cameraY --;
+		
+		camera.translate(cameraX, cameraY);
+		camera.update();
 	}
 	
 	@Override
 	public void dispose () {
 		batch.dispose();
 		img.dispose();
+	}
+	
+	boolean getKey(int key) {
+		return(Gdx.input.isKeyPressed(key));
 	}
 }
