e0e6aec5c26443d2be310811c12d2efe47ab45c4
aree-vanier <gregory@ajayinkingston.com>
2019-02-08 22:37:07 -0500

    Added simple JFrame code to Main.java

--- src/ca/gkelly/culminating/Main.java
+++ src/ca/gkelly/culminating/Main.java
@@ -1,9 +1,38 @@
 package ca.gkelly.culminating;
 
+import java.awt.Color;
+import java.awt.Graphics;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 import ca.gkelly.culminating.loader.Loader;
 
-public class Main{
+public class Main extends JFrame{
 
+	public static JPanel panel;
+	final String NAME = "FLEET";
+	
+	
+	public Main() {
+		
+		setName(NAME);
+		setSize(1280,720);
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		panel = new JPanel();
+		setContentPane(panel);
+		
+		setVisible(true);
+	}
+	
+	@Override
+	public void paint(Graphics g) {
+		g.setColor(Color.WHITE);
+		g.fillRect(0, 0, getWidth(), getHeight());
+		
+	}
+	
 	public static void main(String[] args) {
 		Loader.directory = args[0];
 		
@@ -12,6 +41,8 @@ public class Main{
 		Loader.load();
 		
 		System.out.println(Loader.vessels.get(0).name);
+		
+		new Main();
 		
 	}
 	
